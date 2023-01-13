ba7f8734eaa2c56dff79916da3ee65dea6813a0f
hajaa001 <89516818+hajaa001@users.noreply.github.com>
2023-01-13 13:52:36 +0100

    mmoooo

---
+++ python34.py
@@ -0,0 +1,9 @@
+a = int(input("skriv in tall: "))
+b = int(input("skriv in tall: "))
+c = int(input("skriv in tall: "))
+if a > b and a > c:
+    print(a)
+elif b > a and b > c:
+    print(b)
+elif c > a and c > b:
+    print(c)
---
+++ python35.py
@@ -0,0 +1,13 @@
+a = int(input("skriv in tall: "))
+
+if 90-100:
+    print("A")
+elif 80-89:
+    print("B")
+elif 70-79:
+    print("C")
+elif 60-69:
+    print("D")
+elif 60:
+    print("F")
+   
